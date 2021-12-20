f35fd346f7bd803a7a4ea53ecdc368f12773c645
richlaverty <rich.laverty@gmail.com>
2021-12-20 17:57:06 -0500

    raw data is downloaded and unzipped

--- run_analysis.R
+++ run_analysis.R
@@ -11,3 +11,12 @@
 #
 # 5. From the data set in step 4, creates a second, independent tidy data
 #    set with the average of each variable for each activity and each subject.
+
+setwd("/home/rich/Documents/programs/data_science/03_Getting_and_Cleaning_Data/Week_4/Getting_and_Cleaning_Data_Project")
+if(!file.exists("data")){dir.create("data")}
+if(!file.exists("./data/raw_data.zip"))
+{
+  download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip", destfile = "./data/raw_data.zip")
+  unzip(zipfile = "./data/raw_data.zip", exdir = "./unzipped")
+}
+
